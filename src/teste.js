66b1f519b51cab1a967171de7219dc1a329399ac
Joann Emawodia <108234693+JoannEmawodia@users.noreply.github.com>
2025-09-15 09:29:43 -0300

    Add files via upload

---
+++ src/teste.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
+function mutiplicacao(x, y) {
+    return x * y;
+}
+console.log(mutiplicacao(2, 2));
+//função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome
+function saudacao(nome) {
+    return `Olá, ${nome}`;
+}
+console.log(saudacao("Marcia"));
+//# sourceMappingURL=teste.js.map
---
+++ src/teste.ts
@@ -0,0 +1,14 @@
+//função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
+function mutiplicacao(x:number, y:number):number{
+    return x*y;
+}
+
+console.log(mutiplicacao(2,2));
+
+
+//função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome
+function saudacao(nome: string):string{
+return `Olá, ${nome}`;
+}
+
+console.log(saudacao("Marcia"));
